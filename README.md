0685701a34e571b0c2b571fe4cc86338a37da7b5
ProjextSoft-STUDIONIONS <projectsoft2009@yandex.ru>
2026-01-24 22:48:37 +0400

    update

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Плагин подключения Emoji в редактор TinyMCE4 административной пенели EvolutionCMS
 
 ![Скрин Плагина notocolorimoji в TinyMCE4](src/screen.png)
+
+[NotoCoorEmoji-TinyMCE4](https://github.com/ProjectSoft-STUDIONIONS/NotoCoorEmoji-TinyMCE4)
--- assets/plugins/utilites/notocoloremoji/plugin.notocoloremoji.php
+++ assets/plugins/utilites/notocoloremoji/plugin.notocoloremoji.php
@@ -5,7 +5,7 @@
  * Подключение Noto Color Emoji в админке
  *
  * @category     plugin
- * @version      2.1.0
+ * @version      1.0.0
  * @package      evo
  * @internal     @events OnDocFormRender,OnManagerTopPrerender
  * @internal     @properties 
@@ -16,7 +16,7 @@
  * @license      https://github.com/ProjectSoft-STUDIONIONS/NotoColorEmoji/blob/main/LICENSE GNU General Public License v3.0 (GPL-3.0)
  * @reportissues https://github.com/ProjectSoft-STUDIONIONS/NotoColorEmoji/issues
  * @author       Чернышёв Андрей aka ProjectSoft <projectsoft2009@yandex.ru>
- * @lastupdate   2026-01-24 21:15:54
+ * @lastupdate   2026-01-24 22:47:59
  */
 
 if (!defined('MODX_BASE_PATH')):
